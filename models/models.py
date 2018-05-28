cc2052ae853d1ae5e0113b41dceffe83b2ab9da0
Asier <abilbatm@gmail.com>
2018-05-28 19:37:03 +0000

    Añadir incidencias

--- models/models.py
+++ models/models.py
@@ -3,15 +3,14 @@
 from odoo import models, fields, api
 
 class Ordenador(models.Model):
-     _name = 'pcinventaro.ordenador'
+     _name = 'pcinventario.ordenador'
 
      name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
      description = fields.Text()
+     ubicacion = fields.Char()
 
      session_ids = fields.One2many(
-        'pcinventaro.componente', 'ordenador', string="Sessions")
+        'pcinventario.componente', 'ordenador', string="Sessions")
 
      @api.depends('value')
      def _value_pc(self):
@@ -19,16 +18,30 @@ class Ordenador(models.Model):
 
 
 class Componente(models.Model):
-     _name = 'pcinventaro.componente'
+     _name = 'pcinventario.componente'
      name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+     description = fields.Text(required=False)
 
-     ordenador = fields.Many2one('pcinventaro.ordenador',
+     ordenador = fields.Many2one('pcinventario.ordenador',
         ondelete='cascade', string="Ordenador", required=True)
 
-     @api.depends('value')
-     def _value_pc(self):
-         self.value2 = float(self.value) / 100
+     categoria = fields.Many2one('pcinventario.categoria',
+        ondelete='cascade', string="Categorias", required=True)
+
+
+class Categoria(models.Model):
+     _name = 'pcinventario.categoria'
+     name = fields.Char()
+     description = fields.Text(required=False)
+
+     componentes = fields.One2many(
+        'pcinventario.componente', 'categoria', string="Componentes", required=False)
+
+
+class Incidencia(models.Model):
+     _name = 'pcinventario.incidencia'
+     name = fields.Char()
+     description = fields.Text(required=False)
+     arreglada = fields.Boolean("Arreglada")
+     ordenador = fields.Many2many('pcinventario.ordenador', string="Ordenadores")
 
